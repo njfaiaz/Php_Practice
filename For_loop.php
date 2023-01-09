39a9497cc85c3756b5ade7a8d2a254e2e7df9f1b
NjFaiaz <njfaiaz@gmail.com>
2023-01-09 23:51:07 +0600

    For_Loop

---
+++ For_loop.php
@@ -0,0 +1,12 @@
+<?php 
+for ($a=1; $a <= 10; $a++) { 
+    echo $a .". Faiaz <br>";     // . ei ta concatenate ba pluse korar jonno 
+}
+?>
+
+<br><br>
+<?php 
+for ($a=10; $a >= 1; $a--) { 
+    echo $a .". opp <br>";     // . ei ta concatenate ba pluse korar jonno 
+}
+?>
