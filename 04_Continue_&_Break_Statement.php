553f57e89d5e839c366806711c646acf730bb64a
NjFaiaz <njfaiaz@gmail.com>
2023-01-10 00:29:10 +0600

    Continue_&_Break_Statement

---
+++ 04_Continue_&_Break_Statement.php
@@ -0,0 +1,10 @@
+<?php 
+for ($i=1; $i <=10 ; $i++) { 
+    if($i == 3){
+        echo "No. " . $i . "<br>";
+        continue;
+        //break; break diel ei khanei sob kajta stop hoye zabe ar nice giye baki kaj korbe na 
+    }
+    echo "Number: ". $i . "<br>";
+}
+?>
